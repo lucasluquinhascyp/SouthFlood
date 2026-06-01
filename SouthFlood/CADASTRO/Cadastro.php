d08dffed9ba7f7a8242573fccd4856dafac1b890
CyprianiDev <cyprianilucasrian@gmail.com>
2026-06-01 10:40:44 -0300

    versao 0.0

---
+++ SouthFlood/CADASTRO/Cadastro.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="Cadastro.css">
+    <?php include '../includes/include.php'; ?>
+
+    
+</head>
+<body>
+    <form action="" method="post">
+    <div class="titulo">
+        <h1><p id="Letreiro">Cadastro</p></h1>
+    </div>
+
+    <div class="quadrado-fixo"></div> <!--- quadrado !---->
+    
+    <div class="input-field">
+         <input type="text" name="" id="" class="input"  placeholder="Criar Nome de Usúario"><br>
+        <input type="email" name="" id="" class="input"  placeholder="Colocar E-mail de Usúario"><br>
+        <input type="password" name="" id="" class="input" placeholder="Criar Senha de Usúario"><br>
+          <input type="password" name="" id="" class="input" placeholder="Confirmar Senha de Usúario"><br>
+        <input type="submit" value="Cadastro" class="submit" ><br>
+    </div>    
+</form>
+
+
+
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</body>
+</html>
---
+++ SouthFlood/LOGIN/Login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="Login.css">
+    <?php include '../includes/include.php'; ?>
+
+    
+</head>
+<body>
+    <form action="" method="post">
+    <div class="titulo">
+        <h1><p id="Letreiro">login</p></h1>
+    </div>
+
+    <div class="input-field">
+
+        <input type="email" name="" id="" class="input"  placeholder="E-mail de Usúario"><br>
+        <input type="password" name="" id="" class="input" placeholder="Senha de Usúario"><br>
+        <input type="submit" value="Logar" class=" submit" ><br>
+    </div>    
+</form>
+
+
+
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</body>
+</html>
